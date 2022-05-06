e104d69ef54c205d00475205b3d5ab00db53a23f
William Pratt <willpratt78@gmail.com>
2022-05-06 08:59:27 -0700

    good fail for first test

---
+++ lib/anagrams.rb
@@ -0,0 +1,12 @@
+class Anagram
+  
+  def initualize(input1, input2)
+    @input1 = input1
+    @input2 = input2
+
+  end
+
+  def anagram_checker
+    
+  end
+end
---
+++ spec/anagrams_spec.rb
@@ -0,0 +1,11 @@
+require('rspec')
+require('pry')
+require('anagrams')
+
+
+describe('#anagram_checker') do
+  it('should take a and a and return true') do
+    inputs = Anagram.new("a", "a")
+    expect(inputs.anagram_checker).to(eq(true))
+  end
+end
