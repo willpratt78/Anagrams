0a7a80bac77ded291b9e7638df5001a097fee1c4
William Pratt <willpratt78@gmail.com>
2022-05-06 09:39:41 -0700

    test 3 passing

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -9,13 +9,19 @@ class Anagram
   end
 
   def anagram_checker
-    input1_chars = @input1.chars
-    input2_chars = @input2.chars
 
-    imput1_sorter = input1_chars.sort
+    input1_lower = @input1.downcase
+    input2_lower = @input2.downcase
+    
+    input1_chars = input1_lower.chars
+    input2_chars = input2_lower.chars
+
+    input1_sorter = input1_chars.sort
     input2_sorter = input2_chars.sort
    
-    if imput1_sorter === input2_sorter
+
+
+    if input1_sorter === input2_sorter
       true
     end
   end
