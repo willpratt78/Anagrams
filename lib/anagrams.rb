e8f1e48933ac7c62d093c38d79182a6f48fa76e7
William Pratt <willpratt78@gmail.com>
2022-05-06 09:14:54 -0700

    getting wrong number of arguments error troubleshooting

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,4 +1,6 @@
 class Anagram
+
+  attr_accessor(:input1, :input2)
   
   def initualize(input1, input2)
     @input1 = input1
@@ -7,6 +9,8 @@ class Anagram
   end
 
   def anagram_checker
-    
+    if @input1 === @input2
+      true
+    end
   end
 end
