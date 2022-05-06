3df43da659ae85f97379e932422b5d45b52ee6ef
William Pratt <willpratt78@gmail.com>
2022-05-06 15:24:03 -0700

    updated vowel checker method need to rearrange if else statement

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -13,8 +13,9 @@ class Anagram
     input1_lower = @input1.downcase.gsub(/[^a-z\s]/, '')
     input2_lower = @input2.downcase.gsub(/[^a-z\s]/, '')
 
-    # input1_split = @input1.split(' ')
-    # input2_split = @input2.split(' ')
+    vowels = ['a','e','i','o','u','y']
+    input1_array = []
+    input2_array = []
 
     input1_chars = input1_lower.chars
     input2_chars = input2_lower.chars
@@ -22,23 +23,15 @@ class Anagram
     input1_sorter = input1_chars.sort
     input2_sorter = input2_chars.sort
 
-    input1_vowel_count = 0
-    input2_vowel_count = 0
+    input1.each_char do |letter|
+      input1_array.push(vowels.include?(letter.downcase))
+    end
 
+      input2.each_char do |letter|
+        input2_array.push(vowels.include?(letter.downcase))
+      end
 
-input1_sorter.each do |array_element|
-  if array_element.include?("a") || array_element.include?("e") || array_element.include?("i") || array_element.include?("o") || array_element.include?("u")
-    input1_vowel_count += 1
-  end
-end
-
-input2_sorter.each do |array_element|
-  if array_element === "a" || array_element === "e" || array_element === "i" || array_element === "o" || array_element === "u" || array_element === "y"
-    input2_vowel_count += 1
-  end
-end
-
-    if input1_vowel_count === 0 || input2_vowel_count === 0
+    if input1_array.length != input2_array.length
       "Please input a real word"
     elsif (input1_sorter - input2_sorter) === input1_sorter
       "This is an antigram"
