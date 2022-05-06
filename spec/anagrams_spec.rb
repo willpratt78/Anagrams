e58f45d88d958699d2b9d64a47063c4815b0bebb
William Pratt <willpratt78@gmail.com>
2022-05-06 09:25:20 -0700

    test 2 good fai

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -8,4 +8,8 @@ describe('#anagram_checker') do
     inputs = Anagram.new("a", "a")
     expect(inputs.anagram_checker).to(eq(true))
   end
+  it('should take a and a and return true') do
+    inputs = Anagram.new("ba", "ab")
+    expect(inputs.anagram_checker).to(eq(true))
+  end
 end
