4004a75b6583a01fcc8625fbfb4e7de9532a1cd9
William Pratt <willpratt78@gmail.com>
2022-05-15 20:53:32 -0700

    all tests are now passing

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,9 +1,40 @@
 class Anagram
   
   def initialize(input1, input2)
-    @input1 = input1.downcase.gsub(/\W/, '')
-    @input2 = input2.downcase.gsub(/\W/, '')
+    @input1 = input1.downcase.gsub(/\W/, ' ')
+    @input2 = input2.downcase.gsub(/\W/, ' ')
 
+  end
+
+  def vowel_checker
+    input1_array = @input1.split()
+    input2_array = @input2.split()
+
+    input1_vowel = true
+    input2_vowel = true
+
+    input1_array.each do |array_element|
+      if (array_element.include? 'a') ||(array_element.include? 'e') ||(array_element.include? 'i') ||(array_element.include? 'o') ||(array_element.include?  'u') ||(array_element.include? 'y')
+        input1_vowel = true
+      else
+        input1_vowel = false
+      end
+    end
+
+    input2_array.each do |array_element|
+      if (array_element.include? 'a') ||(array_element.include? 'e') ||(array_element.include? 'i') ||(array_element.include? 'o') ||(array_element.include?  'u') ||(array_element.include? 'y')
+        input2_vowel = true
+      else
+        input2_vowel = false
+
+      end
+    end
+
+    if input1_vowel == false || input2_vowel == false
+      false
+    else
+     true
+    end
   end
 
   def anagram_checker
@@ -11,22 +42,7 @@ class Anagram
     input1_sorter = @input1.chars.sort
     input2_sorter = @input2.chars.sort
 
-    input1_vowel_count = 0
-    input2_vowel_count = 0
-
-input1_sorter.each do |array_element|
-  if array_element === "a" || array_element === "e" || array_element === "i" || array_element === "o" || array_element === "u" || array_element === "y"
-    input1_vowel_count += 1
-  end
-end
-
-input2_sorter.each do |array_element|
-  if array_element === "a" || array_element === "e" || array_element === "i" || array_element === "o" || array_element === "u" || array_element === "y"
-    input2_vowel_count += 1
-  end
-end
-
-    if input1_vowel_count <= 0 || input2_vowel_count <= 0
+    if vowel_checker == false 
       "Please input a real word"
     elsif (input1_sorter - input2_sorter) === input1_sorter
       "This is an antigram"
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -4,35 +4,35 @@ require('anagrams')
 
 
 describe('#anagram_checker') do
-  it('should take a and a and return true') do
+  it('should compare if two words are an anagram') do
     inputs = Anagram.new("a", "a")
     expect(inputs.anagram_checker).to(eq("This is an anagram"))
   end
-  it('should take ba and ab and return true') do
+  it('should compare if two words with more than one letter are an anagram') do
     inputs = Anagram.new("ba", "ab")
     expect(inputs.anagram_checker).to(eq("This is an anagram"))
   end
-  it('should take Ba and Ab and return true') do
+  it('should check to see if something is an anagram despite capitalization') do
     inputs = Anagram.new("Ba", "Ab")
     expect(inputs.anagram_checker).to(eq("This is an anagram"))
   end
-  it('should take wR and rW and return please input a real word') do
+  it('should check to see if something is a real word') do
     inputs = Anagram.new("wR", "rW")
     expect(inputs.anagram_checker).to(eq("Please input a real word"))
   end
-  it('should take Tea and Eat and return true') do
+  it('should check to see if two words are anagrams regardless of capitalization') do
     inputs = Anagram.new("Tea", "Eat")
     expect(inputs.anagram_checker).to(eq("This is an anagram"))
   end
-  it('should take Tea and dog and return this is an antigram') do
-    inputs = Anagram.new("Tea", "dog")
+  it('should check to see if two words are antigrams or not') do
+    inputs = Anagram.new("dog", "Tea")
     expect(inputs.anagram_checker).to(eq("This is an antigram"))
   end
-  it('should take The Morse Code and Here come dots! and return this is an anagram') do
+  it('should check if two words are anagrams or not regardless of punctuation') do
     inputs = Anagram.new("The Morse Code!", "Here come dots!")
     expect(inputs.anagram_checker).to(eq("This is an anagram"))
   end
-  it('should take hello and goodbye and return These two words are neither anagrams nor antigrams') do
+  it('should check if two words are anagrams, antigrams, or neither ') do
     inputs = Anagram.new("hello", "goodbye")
     expect(inputs.anagram_checker).to(eq("These words are neither anagrams nor antigrams"))
   end
@@ -40,7 +40,7 @@ describe('#anagram_checker') do
     inputs = Anagram.new("don't anagram", "Not a Grandma")
     expect(inputs.anagram_checker).to(eq("This is an anagram"))
   end
-  it('should take The Morse Code pkl and Here come dots! lkp and return please input a real word') do
+  it('should take two inputs that are anagrams but contain fake words and return please input a real word') do
     inputs = Anagram.new("The Morse Code pkl", "Here come dots! lkp")
     expect(inputs.anagram_checker).to(eq("Please input a real word"))
   end
